eb9f5ed70a6dce00156db797dca9f9454ba00c29
fmolinag24 <fmolinag@unal.edu.co>
2025-03-01 21:31:31 -0500

    ventana de aplicacion

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,1 +1,22 @@
-ECHO est  activado.
+import pygame
+import variables
+pygame.init()
+pygame.display.set_caption("Slot machine")
+run = True
+ventana = pygame.display.set_mode((variables.ancho, variables.alto))
+while run == True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
