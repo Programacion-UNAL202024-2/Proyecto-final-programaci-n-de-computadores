0edf80cb925c15d183b5d4a4ded78d68f4433ca4
fmolinag24 <fmolinag@unal.edu.co>
2025-03-02 13:40:29 -0500

    slot machine versión 0.3
    se implementaron las animaciones previas a imprimir el número

--- Proyecto/funciones.py
+++ Proyecto/funciones.py
@@ -3,8 +3,23 @@ import variables
 import random
 
 
+def show_animation(cuanto):
+    for i in range (cuanto):
+        pygame.time.delay(200)
+        try:
+            variables.screen.fill((255,255,255))
+            variables.screen.blit(variables.frutas[i],(240,200))
+            variables.screen.blit(variables.frutas[i+3],(360,200))
+            variables.screen.blit(variables.frutas[i+5],(460,200))
+        except IndexError:
+            variables.screen.fill((255,255,255))
+            variables.screen.blit(variables.frutas[i],(240,200))
+            variables.screen.blit(variables.frutas[i-2],(360,200))
+            variables.screen.blit(variables.frutas[i-1],(460,200))
+
+        pygame.display.flip()
+
 def start():
-    
     numero_1 = random.randint(0,7)
     numero_2 = random.randint(0,7)
     numero_3 = random.randint(0,7)
--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,7 +1,7 @@
 import pygame
 import variables
 from funciones import start
-
+from funciones import show_animation
 pygame.init()
 pygame.display.set_caption("Slot machine")
 run = True
@@ -12,6 +12,7 @@ while run == True:
             run = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
+                show_animation(8)
                 start()
     pygame.display.update()
 pygame.quit()
