0fafdd0d68cd0bc8b12b721d0f965d1f7e298a0a
fmolinag24 <fmolinag@unal.edu.co>
2025-03-02 12:57:24 -0500

    slot machine versión 0.2.2
    se optimizó el código, pero nada más

--- Proyecto/funciones.py
+++ Proyecto/funciones.py
@@ -4,22 +4,16 @@ import random
 
 
 def start():
+    
     numero_1 = random.randint(0,7)
     numero_2 = random.randint(0,7)
     numero_3 = random.randint(0,7)
 
-
-    imagen1 = pygame.image.load(f"assets//fruticas//f_0{numero_1}.png")
-    primera_imagen = pygame.transform.scale(imagen1,(80,80))
-
-    imagen2 = pygame.image.load(f"assets//fruticas//f_0{numero_2}.png")
-    segunda_imagen = pygame.transform.scale(imagen2,(80,80))
-
-    imagen3 = pygame.image.load(f"assets//fruticas//f_0{numero_3}.png")
-    tercera_imagen = pygame.transform.scale(imagen3,(80,80))
-
     variables.screen.fill((255,255,255))
-    variables.screen.blit(primera_imagen,(240,200))
-    variables.screen.blit(segunda_imagen,(360,200))
-    variables.screen.blit(tercera_imagen,(460,200))
+    variables.screen.blit(variables.frutas[numero_1],(240,200))
+    variables.screen.blit(variables.frutas[numero_2],(360,200))
+    variables.screen.blit(variables.frutas[numero_3],(460,200))
+
+
+
 
--- Proyecto/variables.py
+++ Proyecto/variables.py
@@ -1,9 +1,14 @@
 import pygame
-import random
 
 ancho = 800
 alto = 600
 
 screen = pygame.display.set_mode((ancho, alto))
+
+frutas = []
+for i in range(8):
+    default = pygame.image.load(f"Proyecto//assets//fruticas//f_0{i}.png")
+    new = pygame.transform.scale(default,(80,80))
+    frutas.append(new)
 
 
